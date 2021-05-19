dbb23a704a551aebdb2104cb663505abe387a625
TheKingInDaNorth <tuan.le.vo19@gmail.com>
2021-05-18 21:13:06 -0700

    5,18,21

---
+++ WebApiFundamentals-master/starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name = "Shawn", Occupation = "Teacher" });
+        }
+    }
+}
