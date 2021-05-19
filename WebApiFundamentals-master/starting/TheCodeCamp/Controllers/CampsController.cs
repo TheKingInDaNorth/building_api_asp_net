0b9695851c5ff63ac5f7a82a0c66ba97235b97c4
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-05-19 16:11:33 -0700

    5,19,21

--- WebApiFundamentals-master/starting/TheCodeCamp/Controllers/CampsController.cs
+++ WebApiFundamentals-master/starting/TheCodeCamp/Controllers/CampsController.cs
@@ -1,16 +1,56 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
-        public IHttpActionResult Get()
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
-            return Ok(new { Name = "Shawn", Occupation = "Teacher" });
+            _repository = repository;
+            _mapper = mapper;
+        }
+        [Route()]
+        public async Task<IHttpActionResult> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync(includeTalks);
+
+                //Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker, bool includeTalks = false)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker, includeTalks);
+                if (result == null) return NotFound();
+                return Ok(_mapper.Map<CampModel>(result));
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
     }
 }
